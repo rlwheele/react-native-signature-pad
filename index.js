41f1ef71d7da8fb4797bcd0bdafb49acce7e9753
rlwheeler <info@rlwheeler.com>
2018-07-02 11:26:24 -0400

    'View.propTypes.style' not supported by expo build. Replaced 'View' import with 'ViewPropTypes'

--- index.js
+++ index.js
@@ -2,9 +2,9 @@
 
 import React, { Component, PropTypes } from 'react';
 import {
-  View,
   WebView,
   StyleSheet,
+  ViewPropTypes
 } from 'react-native';
 
 
@@ -19,7 +19,7 @@ class SignaturePad extends Component {
   static propTypes = {
     onChange: PropTypes.func,
     onError: PropTypes.func,
-    style: View.propTypes.style,
+    style: ViewPropTypes.style,
     penColor: PropTypes.string,
     dataURL: PropTypes.string,
   };
